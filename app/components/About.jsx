c61678493cccbe9326702ed6eb9d59833a27ba73
muzammil-shayk <m.muzammil47@protonmail.com>
2025-09-24 16:16:25 +0500

    header and navbar complete

---
+++ app/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-ovo">Introduction</h4>
+      <h2 className="text-center text-5xl font-ovo">About Me</h2>
+    </div>
+  );
+};
+
+export default About;
