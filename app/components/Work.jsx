953c8545706947a1cb3b437dabace22ad2f997ac
muzammil-shayk <m.muzammil47@protonmail.com>
2025-09-25 21:12:13 +0500

    my work darkmode

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -2,7 +2,7 @@ import { assets, workData } from '@/assets/assets';
 import Image from 'next/image';
 import React from 'react';
 
-const Work = () => {
+const Work = ({ isDarkMode }) => {
   return (
     <div id="work" className="w-full px-6 sm:px-[12%] py-10 scroll-mt-20">
       <h4 className="text-center -mt-5 text-lg font-ovo">My Portfolio</h4>
@@ -13,7 +13,7 @@ const Work = () => {
         Welcome to my Portfolio!. Explore a collection of projects showcasing my
         work.
       </p>
-      <div className="grid grid-cols-auto my-10 gap-5">
+      <div className="grid grid-cols-auto my-10 gap-5 dark:text-black">
         {workData.map((project, index) => (
           <div
             key={index}
@@ -34,11 +34,13 @@ const Work = () => {
       </div>
       <a
         href=""
-        className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500"
+        className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white/80 dark:hover:bg-darkHover"
       >
         Show more{' '}
         <Image
-          src={assets.right_arrow_bold}
+          src={
+            isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold
+          }
           alt="right arrow"
           className="w-4"
         />
