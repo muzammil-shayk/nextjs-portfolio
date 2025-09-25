e00d2af7d7764b6a04fd7d647c0ee06452e91291
muzammil-shayk <m.muzammil47@protonmail.com>
2025-09-25 14:40:20 +0500

    footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,9 +4,36 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
+    <div className="mt-20">
       <div className="text-center">
         <Image src={assets.logo} alt="" className="w-36 mx-auto mb-2" />
+        <div className="w-max flex items-center gap-2 mx-auto">
+          <Image src={assets.mail_icon} alt="" className="w-6" />
+          m.muzammil47@protonmail.com
+        </div>
+      </div>
+      <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
+        <p>© 2025 M. Muzammil. All rights reserved.</p>
+        <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
+          <li>
+            <a target="_blank" href="https://github.com/muzammil-shayk">
+              Github
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/muhammad-muzammil-8771a4309/"
+            >
+              Linkedin
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://www.instagram.com/muzammilshayk">
+              Instagram
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
